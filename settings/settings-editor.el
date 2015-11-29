b0a94c3b7ed07d95bec6692bb7ef1d4eadbcc687
Alex Marcotte <alex.marcotte@gmail.com>
2015-11-28 22:19:31 -0500

    Added er/expand-region bound to C-@

--- settings/settings-editor.el
+++ settings/settings-editor.el
@@ -10,7 +10,15 @@
   :bind (("C-c u" . move-text-up)
 	 ("C-c d" . move-text-down))
   )
-  
+
+;; Expand region
+(use-package expand-region
+  :ensure t
+  :bind ("C-@" . er/expand-region)
+  :config
+  (pending-delete-mode t)
+;;  (global-set-key (kbd "C-@") 'er/expand-region)
+  )
   
 
 (provide 'settings-editor)
